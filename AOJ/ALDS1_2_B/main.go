a49e4ac146481bd5eaa17801d98a505b20934544
asuforce <owata.sn@gmail.com>
2019-05-14 13:52:51 +0900

    Clear ALDS1_2_B

---
+++ AOJ/ALDS1_2_B/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+
+	var n int
+	fmt.Fscan(r, &n)
+
+	var e = make([]int, n)
+
+	for x := 0; x < n; x++ {
+		fmt.Fscan(r, &e[x])
+	}
+
+	a, c := selectionSort(e, n)
+	fmt.Printf("%v\n%d\n", strings.Trim(fmt.Sprint(a), "[]"), c)
+}
+
+func selectionSort(e []int, n int) ([]int, int) {
+	cnt := 0
+	for i := 0; i < n; i++ {
+		minj := i
+		for j := i; j < n; j++ {
+			if e[j] < e[minj] {
+				minj = j
+			}
+		}
+		if e[i] != e[minj] {
+			tmp := e[i]
+			e[i] = e[minj]
+			e[minj] = tmp
+			cnt++
+		}
+	}
+	return e, cnt
+}
