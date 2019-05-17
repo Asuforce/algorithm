65e53759d68311434b2154d998ca07e0ab367f40
asuforce <owata.sn@gmail.com>
2019-05-17 16:45:14 +0900

    Implement stack package
    ref: https://godoc.org/github.com/golang-collections/collections/stack

--- AOJ/ALDS1_3_A/main.go
+++ AOJ/ALDS1_3_A/main.go
@@ -8,44 +8,67 @@ import (
 	"strings"
 )
 
-type stack []int
-
-func (s stack) Pop() (stack, int) {
-	l := len(s)
-	return s[:l-1], s[l-1]
+type stack struct {
+	t   *node
+	len int
 }
 
-func (s stack) Push(v int) stack {
-	return append(s, v)
+type node struct {
+	v    int
+	prev *node
+}
+
+func (s *stack) Len() int {
+	return s.len
+}
+
+func (s *stack) Peek() int {
+	if s.len == 0 {
+		return -1
+	}
+	return s.t.v
+}
+
+func (s *stack) Pop() int {
+	if s.len == 0 {
+		return -1
+	}
+	n := s.t
+	s.t = n.prev
+	s.len--
+	return n.v
+}
+
+func (s *stack) Push(v int) {
+	s.t = &node{v: v, prev: s.t}
+	s.len++
+}
+
+func (s *stack) calc(op string) {
+	l, r := s.Pop(), s.Pop()
+	switch op {
+	case "+":
+		s.Push(l + r)
+	case "-":
+		s.Push(r - l)
+	case "*":
+		s.Push(l * r)
+	}
 }
 
 func main() {
 	s := bufio.NewScanner(os.Stdin)
-
 	e := []string{}
 	if s.Scan() {
-		e = strings.Split(s.Text(), " ")
+		e = strings.Fields(s.Text())
 	}
-
-	l, r := 0, 0
-	f := make(stack, 0)
-
+	f := &stack{nil, 0}
 	for _, ele := range e {
 		if num, err := strconv.Atoi(ele); err == nil {
-			f = f.Push(num)
+			f.Push(num)
 		} else {
-			f, l = f.Pop()
-			f, r = f.Pop()
-			switch ele {
-			case "+":
-				f = f.Push(l + r)
-			case "-":
-				f = f.Push(r - l)
-			case "*":
-				f = f.Push(l * r)
-			}
+			f.calc(ele)
 		}
 	}
-
-	fmt.Println(strings.Trim(fmt.Sprint(f), "[]"))
+	fmt.Println(f.Peek())
 }
