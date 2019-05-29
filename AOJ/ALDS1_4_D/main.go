6eccba13d69134268e34a53256bef1765ba0aa3e
asuforce <owata.sn@gmail.com>
2019-05-29 13:48:15 +0900

    Tububg code

--- AOJ/ALDS1_4_D/main.go
+++ AOJ/ALDS1_4_D/main.go
@@ -7,40 +7,32 @@ import (
 	"strconv"
 )
 
-var s = bufio.NewScanner(os.Stdin)
-
-const (
-	MAX_PACKAGES = 100000
-	MAX_TRACKS   = 100000
+var (
+	s       = bufio.NewScanner(os.Stdin)
+	n, k, w = 0, 0, []int{}
 )
 
-type allocation struct {
-	n int
-	k int
-	w []int
-}
-
-func (a *allocation) check(p int) int {
+func check(p int) int {
 	i := 0
-	for j := 0; j < a.k; j++ {
+	for j := 0; j < k; j++ {
 		s := 0
-		for s+a.w[i] <= p {
-			s += a.w[i]
+		for s+w[i] <= p {
+			s += w[i]
 			i++
-			if i == a.n {
-				return a.n
+			if i == n {
+				return n
 			}
 		}
 	}
 	return i
 }
 
-func (a *allocation) binarySearch() int {
-	left, mid, right := 0, 0, MAX_PACKAGES*MAX_TRACKS
+func binarySearch() int {
+	left, right := 0, 100000*100000
 	for right-left > 1 {
-		mid = (left + right) / 2
-		v := a.check(mid)
-		if v >= a.n {
+		mid := (left + right) / 2
+		v := check(mid)
+		if v >= n {
 			right = mid
 			continue
 		}
@@ -52,22 +44,21 @@ func (a *allocation) binarySearch() int {
 func main() {
 	s.Split(bufio.ScanWords)
 
-	a := &allocation{}
-	a.n = nextInt(s)
-	a.k = nextInt(s)
-	a.w = make([]int, a.n)
+	n = nextInt(s)
+	k = nextInt(s)
+	w = make([]int, n)
 
-	for i := range a.w {
-		a.w[i] = nextInt(s)
+	for i := range w {
+		w[i] = nextInt(s)
 	}
-	fmt.Println(a.binarySearch())
+	fmt.Println(binarySearch())
 }
 
 func nextInt(s *bufio.Scanner) int {
 	s.Scan()
-	n, err := strconv.Atoi(s.Text())
+	r, err := strconv.Atoi(s.Text())
 	if err != nil {
 		panic(err)
 	}
-	return n
+	return r
 }
